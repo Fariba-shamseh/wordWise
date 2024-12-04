b0ce5b9726eea16041a26a65cf149bdc68c9bed9
Fariba Shamseh <fariba.shamseh0@gmail.com>
2024-12-04 18:11:47 +0330

    building the page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import style from "./HomePage.module.css"
+export default function HomePage() {
+  return (
+<main className={style.homepage}>
+    <section>
+        <h1>
+            You travel the world.
+            <br/>
+            WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+            A world map that tracks your footsteps into every city you can think
+            of. Never forget your wonderful experiences, and show your friends how
+            you have wandered the world.
+        </h2>
+    </section>
+</main>
+  );
+}
+
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>page not found 😓</h1>
+    </div>
+  );
+}
+
